f42878ad04800eb66d107097ac4a618a65e137f8
Daniél Kerkmann <daniel@kerkmann.dev>
2022-09-18 07:43:01 +0200

    add todo_comments (#1)

---
+++ after/plugin/_todo_comments.lua
@@ -0,0 +1,28 @@
+L("todo-comments", function(todo_comments)
+  todo_comments.setup()
+
+  L("telescope", function()
+    Map("n", "<Leader>tf", ":TodoTelescope keywords=FIX<CR>", { silent = true })
+    Map("n", "<Leader>th", ":TodoTelescope keywords=HACK<CR>", { silent = true })
+    Map("n", "<Leader>tn", ":TodoTelescope keywords=NOTE<CR>", { silent = true })
+    Map("n", "<Leader>tp", ":TodoTelescope keywords=PERF<CR>", { silent = true })
+    Map("n", "<Leader>tt", ":TodoTelescope<CR>", { silent = true })
+    Map("n", "<Leader>tw", ":TodoTelescope keywords=WARNING<CR>", { silent = true })
+    Map("n", "<Leader>tT", ":TodoTelescope keywords=TODO<CR>", { silent = true })
+
+    L("which-key", function(wk)
+      wk.register({
+        t = {
+          name = "+todo",
+          f = { "Show Fix" },
+          h = { "Show Hack" },
+          n = { "Show Note" },
+          p = { "Show Perf" },
+          t = { "Show All " },
+          w = { "Show Warning" },
+          T = { "Show Todo" },
+        },
+      }, { prefix = "<leader>" })
+    end)
+  end)
+end)
