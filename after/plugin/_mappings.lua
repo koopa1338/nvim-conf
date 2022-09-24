093ea7edfc687604180b57cf3676335e5d3f52ab
Daniél Kerkmann <daniel@kerkmann.dev>
2022-09-25 01:07:57 +0200

    add CTR-BS as CTRL-W to the mappings

--- after/plugin/_mappings.lua
+++ after/plugin/_mappings.lua
@@ -64,6 +64,10 @@ Map("n", "Y", "y$", { remap = false })
 
 Map("n", "<leader><leader>m", ":Messages<CR>", { silent = true })
 
+-- editing
+-- <C-H> is <C-BS>, see: https://vi.stackexchange.com/questions/16139/s-bs-and-c-bs-mappings-not-working
+Map("i", "<C-H>", "<C-W>", {})
+
 L("which-key", function(wk)
   wk.register({
     b = {
