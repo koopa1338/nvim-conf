5d8f90f84a9ff68bef90cc45c907f1db71d38dcb
Daniél Kerkmann <daniel@kerkmann.dev>
2022-09-24 02:03:58 +0200

    change dap bindings

--- after/plugin/_dap.lua
+++ after/plugin/_dap.lua
@@ -4,50 +4,66 @@ if not dap or not dapui then
   return
 end
 
-Map("n", "<F4>", dap.toggle_breakpoint, { silent = true })
--- <S-F4>
-Map("n", "<F16>", function()
+Map("n", "<leader>Db", dap.toggle_breakpoint, { silent = true })
+Map("n", "<leader>DB", function()
   dapui.float_element("breakpoints", { enter = true })
 end, { silent = true })
--- <C-F4>
-Map("n", "<F28>", function()
+Map("n", "<leader>Dc", function()
   vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
     dap.set_breakpoint(input)
   end)
 end, { silent = true })
--- g<C-F4>
-Map("n", "g<F28>", function()
+Map("n", "<leader>Dm", function()
   vim.ui.input({ prompt = "Log Point message: " }, function(input)
     dap.set_breakpoint(nil, nil, input)
   end)
 end, { silent = true })
 
-Map("n", "<F5>", dap.continue, { silent = true })
--- <S-F5>
-Map("n", "<F17>", dap.pause, { silent = true })
+Map("n", "<M-d>", dap.continue, { silent = true })
+Map("n", "<leader>Dp", dap.pause, { silent = true })
 
-Map("n", "<F6>", function()
+Map("n", "<leader>Ds", function()
   dapui.float_element("stacks", { enter = true })
 end, { silent = true })
--- <S-F6>
-Map("n", "<F18>", function()
+Map("n", "<leader>Dt", function()
   dapui.float_element("console", { enter = true })
 end, { silent = true })
 
-Map("n", "<F9>", dap.step_over, { silent = true })
--- <S-F9>
-Map("n", "<F21>", dap.goto_, { silent = true })
+Map("n", "<leader>Do", dap.step_over, { silent = true })
+Map("n", "<leader>Dg", dap.goto_, { silent = true })
 
-Map("n", "<F10>", dap.step_into, { silent = true })
--- <S-F10>
-Map("n", "<F22>", dap.step_out, { silent = true })
+Map("n", "<leader>Dl", dap.step_into, { silent = true })
+Map("n", "<leader>Dh", dap.step_out, { silent = true })
 
-Map("n", "<F11>", dap.down, { silent = true })
--- <S-F11>
-Map("n", "<F23>", dap.up, { silent = true })
+Map("n", "<leader>Dk", dap.up, { silent = true })
+Map("n", "<leader>Dj", dap.down, { silent = true })
 
-Map("n", "<F12>", dap.repl.toggle, { silent = true })
-Map("n", "<F24>", dap.disconnect, { silent = true })
+Map("n", "<leader>Dr", dap.repl.toggle, { silent = true })
+Map("n", "<leader>DD", dap.disconnect, { silent = true })
+
+L("which-key", function(wk)
+  wk.register({
+    D = {
+      name = "+Debug",
+      b = { "Toggle Debug Breakpoint" },
+      B = { "Show Debug Breakpoints" },
+      c = { "Set Debug Breakpoint Condition" },
+      m = { "Set Debug Log Point Message" },
+      p = { "Pause Debugger" },
+      s = { "Show Debug Stacks" },
+      t = { "Show Debug Console" },
+      o = { "Debug Step Over" },
+      g = { "Debug GoTo" },
+      l = { "Debug Step Into" },
+      h = { "Debug Step Out" },
+      k = { "Debug Step Up" },
+      j = { "Debug Step Down" },
+      r = { "Toggle Debug REPL" },
+      D = { "Disconnect Debugger" },
+      ["<M-d"] = { "Continue Debugger" },
+    },
+  }, { prefix = "<leader>" })
+end)
 
 -- TODO: setup configurations for debug options
 -- dap.configuration.rust = {}
