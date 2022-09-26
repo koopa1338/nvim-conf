6d31609ee7df62bfb569d55319a141c8a8452b04
koopa1338 <sinner1991@gmail.com>
2022-09-27 01:15:42 +0200

    lower timeout for notifications

--- after/plugin/_notify.lua
+++ after/plugin/_notify.lua
@@ -1,6 +1,7 @@
 L("notify", function(notify)
   notify.setup {
     stages = "slide",
+    timeout = 1500,
     on_open = function(win)
       if vim.api.nvim_win_is_valid(win) then
         vim.api.nvim_win_set_config(win, { border = vim.g.border_type })
