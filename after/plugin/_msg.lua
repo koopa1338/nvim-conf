22763d34a834e8e87018033ce2d5850e72115471
koopa1338 <sinner1991@gmail.com>
2022-10-06 18:37:55 +0200

    centered floating window for messages popup

--- after/plugin/_msg.lua
+++ after/plugin/_msg.lua
@@ -8,6 +8,22 @@ L("messages", function(msg)
       vim.api.nvim_buf_set_option(buf, "filetype", "msgfloat")
       return vim.api.nvim_open_win(buf, true, opts)
     end,
+    buffer_opts = function(lines)
+      local win_height = vim.o.lines - vim.o.cmdheight - 2 -- Add margin for status and buffer line
+      local win_width = vim.o.columns
+      local height = math.floor(win_height * 0.9)
+      local width = math.floor(win_width * 0.8)
+      return {
+        height = height,
+        width = width,
+        row = math.floor((win_height - height) / 2),
+        col = math.floor((win_width - width) / 2),
+        relative = "editor",
+        style = "minimal",
+        border = vim.g.border_type,
+        zindex = 1,
+      }
+    end,
   }
 
   L("messages.api", function(api)
