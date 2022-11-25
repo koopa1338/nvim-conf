749af9d15745f309660395984cefb66b55e413a5
koopa1338 <sinner1991@gmail.com>
2022-11-25 18:22:27 +0100

    fix surround aliases

--- after/plugin/_surround.lua
+++ after/plugin/_surround.lua
@@ -1,3 +1,8 @@
 L("nvim-surround", function(surround)
   surround.setup {}
+  -- see help for nvim-surround.aliasing
+  Map("o", "ir", "i[", { remap = false })
+  Map("o", "ar", "a[", { remap = false })
+  Map("o", "ia", "i<", { remap = false })
+  Map("o", "aa", "a<", { remap = false })
 end)
