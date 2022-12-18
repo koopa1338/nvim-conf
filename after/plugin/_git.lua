f7918fdf219519922771d16fa7af7d99935089bd
koopa1338 <sinner1991@gmail.com>
2022-12-18 01:24:43 +0100

    plugin update diffview

--- after/plugin/_git.lua
+++ after/plugin/_git.lua
@@ -98,22 +98,24 @@ L("diffview", function(diffview)
           height = 15,
         },
         log_options = {
-          single_file = {
-            max_count = 512, -- Limit the number of commits
-            follow = true, -- Follow renames (only for single file)
-            all = false, -- Include all refs under 'refs/' including HEAD
-            merges = false, -- List only merge commits
-            no_merges = false, -- List no merge commits
-            reverse = false, -- List commits in reverse order
-          },
-          multi_file = {
-            max_count = 128, -- Limit the number of commits
-            follow = false, -- Follow renames (only for single file)
-            all = false, -- Include all refs under 'refs/' including HEAD
-            merges = false, -- List only merge commits
-            no_merges = false, -- List no merge commits
-            reverse = false, -- List commits in reverse order
-          },
+          git = {
+            single_file = {
+              max_count = 512, -- Limit the number of commits
+              follow = true, -- Follow renames (only for single file)
+              all = false, -- Include all refs under 'refs/' including HEAD
+              merges = false, -- List only merge commits
+              no_merges = false, -- List no merge commits
+              reverse = false, -- List commits in reverse order
+            },
+            multi_file = {
+              max_count = 128, -- Limit the number of commits
+              follow = false, -- Follow renames (only for single file)
+              all = false, -- Include all refs under 'refs/' including HEAD
+              merges = false, -- List only merge commits
+              no_merges = false, -- List no merge commits
+              reverse = false, -- List commits in reverse order
+            },
+          }
         },
       },
       default_args = { -- Default args prepended to the arg-list for the listed commands
