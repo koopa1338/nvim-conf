75a1a493c858becb788b7fabdaf4ef5e6e9cd582
koopa1338 <sinner1991@gmail.com>
2023-03-06 15:11:07 +0100

    neogit config: icons, diffview integration

--- lua/plugins/_neogit.lua
+++ lua/plugins/_neogit.lua
@@ -5,8 +5,31 @@ local M = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
   },
+  keys = {
+    { "<leader>gN", ":Neogit<CR>", desc = "Open Neogit UI" },
+  },
 }
 
-M.config = function() end
+M.config = function()
+  local signs = L("signs").signs
+  L("neogit").setup {
+    signs = {
+      section = { signs.FoldClosed.icon, signs.FoldOpen.icon },
+      item = { signs.FoldClosed.icon, signs.FoldOpen.icon },
+      hung = { signs.FoldClosed.icon, signs.FoldOpen.icon },
+    },
+    integrations = {
+      diffview = true,
+    },
+  }
+
+  L("which-key", function(wk)
+    wk.register({
+      g = {
+        N = { "Open Neogit UI" },
+      },
+    }, { prefix = "<leader>" })
+  end)
+end
 
 return M
