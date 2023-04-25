245f9a402e17d54ef697ecbe159598f0d5b7f00b
koopa1338 <sinner1991@gmail.com>
2023-04-25 22:17:12 +0200

    fix null ls

--- lua/plugins/lsp/nullls.lua
+++ lua/plugins/lsp/nullls.lua
@@ -1,16 +1,18 @@
 local M = {}
 
-M.setup = function(null_ls)
+M.setup = function()
   L("lsp_sources", function(lsp_sources)
     lsp_sources.ls = null_ls
     local opts = {
       border = vim.g.border_type,
       sources = lsp_sources.get_null_ls_sources(),
     }
-    L("lsp_utils", function(ls_utils)
+    L("lsp_utils", function(lsp_utils)
       opts.on_attach = lsp_utils.on_attach
     end)
-    null_ls.setup(opts)
+    L("null-ls", function(null_ls)
+      null_ls.setup(opts)
+    end)
   end)
 end
 
