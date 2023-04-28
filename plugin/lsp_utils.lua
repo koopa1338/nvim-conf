7b2f8e3044f62d66ce6fba8713cad5e85e6cbd18
koopa1338 <sinner1991@gmail.com>
2023-04-28 04:28:34 +0200

    feat(lsp): option to register multiple mappings per provider
    also added a mapping for source only code actions

--- plugin/lsp_utils.lua
+++ plugin/lsp_utils.lua
@@ -50,117 +50,160 @@ M.supported = {
 
 local provider_mapping = {
   declarationProvider = {
-    mode = "n",
-    keys = "<leader>lD",
-    callback = vim.lsp.buf.declaration,
-    desc = "Show Declaration",
-    error = "LSP does not support jump to declaration",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>lD",
+      callback = vim.lsp.buf.declaration,
+      desc = "Show Declaration",
+      error = "LSP does not support jump to declaration",
+      buffer = true,
+    },
   },
   definitionProvider = {
-    mode = "n",
-    keys = "<leader>ld",
-    callback = vim.lsp.buf.definition,
-    desc = "Jump to Definition",
-    error = "LSP does not support jump to defenition",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>ld",
+      callback = vim.lsp.buf.definition,
+      desc = "Jump to Definition",
+      error = "LSP does not support jump to defenition",
+      buffer = true,
+    },
   },
   typeDefinitionProvider = {
-    mode = "n",
-    keys = "<leader>lT",
-    callback = vim.lsp.buf.type_definition,
-    desc = "Show Type Definition",
-    error = "LSP does not support show document type definition",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>lT",
+      callback = vim.lsp.buf.type_definition,
+      desc = "Show Type Definition",
+      error = "LSP does not support show document type definition",
+      buffer = true,
+    },
   },
   renameProvider = {
-    mode = "n",
-    keys = "<leader>lr",
-    callback = vim.lsp.buf.rename,
-    desc = "Rename under Cursor",
-    error = "LSP does not support show rename",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>lr",
+      callback = vim.lsp.buf.rename,
+      desc = "Rename under Cursor",
+      error = "LSP does not support show rename",
+      buffer = true,
+    },
   },
   documentFormattingProvider = {
-    mode = "n",
-    keys = "<leader>lf",
-    callback = function()
-      if vim.version().minor >= 8 then
-        vim.lsp.buf.format() -- { async = true }
-      else
-        vim.lsp.buf.formatting()
-      end
-    end,
-    desc = "Format File",
-    error = "LSP does not support formatting",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>lf",
+      callback = function()
+        if vim.version().minor >= 8 then
+          vim.lsp.buf.format() -- { async = true }
+        else
+          vim.lsp.buf.formatting()
+        end
+      end,
+      desc = "Format File",
+      error = "LSP does not support formatting",
+      buffer = true,
+    },
   },
   signatureHelpProvider = {
-    mode = "n",
-    keys = "<leader>ls",
-    callback = vim.lsp.buf.signature_help,
-    desc = "Show Signature Help",
-    error = "LSP does not support signature help",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "<leader>ls",
+      callback = vim.lsp.buf.signature_help,
+      desc = "Show Signature Help",
+      error = "LSP does not support signature help",
+      buffer = true,
+    },
   },
   codeActionProvider = {
-    mode = { "n", "v" },
-    keys = "<leader>lca",
-    callback = vim.lsp.buf.code_action,
-    desc = "Select Code Actions",
-    error = "LSP does not support code actions",
-    buffer = true,
+    {
+      mode = { "n", "v" },
+      keys = "<leader>lcA",
+      callback = function()
+        vim.lsp.buf.code_action {
+          context = {
+            only = {
+              "source",
+            },
+            diagnostics = {},
+          },
+        }
+      end,
+      desc = "Select Code Actions",
+      error = "LSP does not support code actions",
+      buffer = true,
+    },
+    {
+      mode = { "n", "v" },
+      keys = "<leader>lca",
+      callback = vim.lsp.buf.code_action,
+      desc = "Select Code Actions",
+      error = "LSP does not support code actions",
+      buffer = true,
+    },
   },
   hoverProvider = {
-    mode = "n",
-    keys = "K",
-    callback = vim.lsp.buf.hover,
-    desc = "Show Hover Information",
-    error = "LSP does not support hover information",
-    buffer = true,
+    {
+      mode = "n",
+      keys = "K",
+      callback = vim.lsp.buf.hover,
+      desc = "Show Hover Information",
+      error = "LSP does not support hover information",
+      buffer = true,
+    },
   },
   documentSymbolProvider = {
-    mode = "n",
-    keys = "<leader>lts",
-    callback = "<cmd>Telescope lsp_document_symbols<CR>",
-    desc = "Show Document Symbols",
-    error = "LSP does not support showing document symbols",
-    buffer = false,
+    {
+      mode = "n",
+      keys = "<leader>lts",
+      callback = "<cmd>Telescope lsp_document_symbols<CR>",
+      desc = "Show Document Symbols",
+      error = "LSP does not support showing document symbols",
+      buffer = false,
+    },
   },
   workspaceSymbolProvider = {
-    mode = "n",
-    keys = "<leader>ltS",
-    callback = "<cmd>Telescope lsp_workspace_symbols<CR>",
-    desc = "Show Workspace Symbols",
-    error = "LSP does not support showing workspace symbols",
-    buffer = false,
+    {
+      mode = "n",
+      keys = "<leader>ltS",
+      callback = "<cmd>Telescope lsp_workspace_symbols<CR>",
+      desc = "Show Workspace Symbols",
+      error = "LSP does not support showing workspace symbols",
+      buffer = false,
+    },
   },
   referencesProvider = {
-    mode = "n",
-    keys = "<leader>ltr",
-    callback = "<cmd>Telescope lsp_references<CR>",
-    desc = "Show References",
-    error = "LSP does not support showing references",
-    buffer = false,
+    {
+      mode = "n",
+      keys = "<leader>ltr",
+      callback = "<cmd>Telescope lsp_references<CR>",
+      desc = "Show References",
+      error = "LSP does not support showing references",
+      buffer = false,
+    },
   },
   implementationProvider = {
-    mode = "n",
-    keys = "<leader>lti",
-    callback = "<cmd>Telescope lsp_implementations<CR>",
-    desc = "Show Implementations",
-    error = "LSP does not support showing implementations",
-    buffer = false,
+    {
+      mode = "n",
+      keys = "<leader>lti",
+      callback = "<cmd>Telescope lsp_implementations<CR>",
+      desc = "Show Implementations",
+      error = "LSP does not support showing implementations",
+      buffer = false,
+    },
   },
 }
 
 local map_providers = function(capabilities, bufnr)
-  for provider, prov_opts in pairs(provider_mapping) do
-    local opts = { silent = true, desc = prov_opts.desc }
-    if prov_opts.buffer then
-      opts.buffer = bufnr
-    end
+  for provider, mappings in pairs(provider_mapping) do
     if capabilities[provider] then
-      Map(prov_opts.mode, prov_opts.keys, prov_opts.callback, opts)
+      for _, map in ipairs(mappings) do
+        local opts = { silent = true, desc = map.desc }
+        if map.buffer then
+          opts.buffer = bufnr
+        end
+        Map(map.mode, map.keys, map.callback, opts)
+      end
       M.supported[provider] = true
     end
   end
@@ -170,9 +213,11 @@ local map_unsupported = function()
   for provider, supported in pairs(M.supported) do
     if not supported then
       local prov = provider_mapping[provider]
-      Map(prov.mode, prov.keys, function()
-        notify_unsupported_lsp(prov.error, prov.desc)
-      end, { silent = true, desc = prov.desc })
+      for _, map in ipairs(prov) do
+        Map(map.mode, map.keys, function()
+          notify_unsupported_lsp(map.error, map.desc)
+        end, { silent = true, desc = map.desc })
+      end
     end
   end
 end
