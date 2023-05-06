1d0a9bf9d574e7c87c5f404dd4c55012f9692ed6
koopa1338 <sinner1991@gmail.com>
2023-05-06 16:38:26 +0200

    refactor(trouble): add, change some options and fixed mappings

--- lua/plugins/_trouble.lua
+++ lua/plugins/_trouble.lua
@@ -8,10 +8,17 @@ M.config = function()
   L("trouble", function(trouble)
     trouble.setup {
       auto_preview = false,
-      auto_fold = true,
+      auto_fold = false,
       auto_close = true,
       use_diagnostic_signs = true,
       height = 15,
+      padding = false,
+      fold_open = Get_sign_def("FoldOpen").icon,
+      fold_closed = Get_sign_def("FoldClosed").icon,
+      action_keys = {
+        jump = "o",
+        jump_close = { "<cr>", "<tab>" },
+      },
     }
 
     Map("n", "<leader>Tt", "<cmd>Trouble<CR>", { silent = true, desc = "Show document diagnostics" })
