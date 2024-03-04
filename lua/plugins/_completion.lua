1c7794de9602e438adf9a52bcb64045410ae8926
koopa1338 <sinner1991@gmail.com>
2024-03-04 03:03:23 +0100

    use c-n/p for completion navigation

--- lua/plugins/_completion.lua
+++ lua/plugins/_completion.lua
@@ -108,28 +108,10 @@ M.config = function()
         select = true,
       },
       -- navigate completion menu
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
       -- navigate snippet positions
-      ["<C-f>"] = cmp.mapping(function(fallback)
+      ["<C-l>"] = cmp.mapping(function(fallback)
         if luasnip.expand_or_locally_jumpable() then
           luasnip.expand_or_jump()
         elseif has_words_before() then
@@ -141,8 +123,8 @@ M.config = function()
         "i",
         "s",
       }),
-      ["<C-b>"] = cmp.mapping(function(fallback)
-        if luasnip.jumpable(-1) then
+      ["<C-h>"] = cmp.mapping(function(fallback)
+        if luasnip.locally_jumpable(-1) then
           luasnip.jump(-1)
         else
           fallback()
