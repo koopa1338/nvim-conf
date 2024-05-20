8398922c690158c5690715bbabfffce4ee9eaa78
koopa1338 <sinner1991@gmail.com>
2024-05-20 20:28:09 +0200

    experiment with vim.snippet from 0.10

--- lua/plugins/_completion.lua
+++ lua/plugins/_completion.lua
@@ -9,10 +9,9 @@ local M = {
     {
       "MattiasMTS/cmp-dbee",
       dependencies = {
-        { "kndndrj/nvim-dbee" }
+        { "kndndrj/nvim-dbee" },
       },
       ft = "sql", -- optional but good to have
-
     },
   },
   event = "InsertEnter",
@@ -20,14 +19,45 @@ local M = {
 
 M.config = function()
   local cmp = L "cmp"
-  local luasnip = L "luasnip"
+  local ls = L "luasnip"
   local lspkind = L "lspkind"
 
-  local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+  vim.snippet.expand = ls.lsp_expand
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.snippet.active = function(filter)
+    filter = filter or {}
+    filter.direction = filter.direction or 1
+
+    if filter.direction == 1 then
+      return ls.expand_or_jumpable()
+    else
+      return ls.jumpable(filter.direction)
+    end
   end
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.snippet.jump = function(direction)
+    if direction == 1 then
+      if ls.expandable() then
+        return ls.expand_or_jump()
+      else
+        return ls.jumpable(1) and ls.jump(1)
+      end
+    else
+      return ls.jumpable(-1) and ls.jump(-1)
+    end
+  end
+
+  vim.snippet.stop = ls.unlink_current
+
+  Map({ "i", "s" }, "<c-l>", function()
+    return vim.snippet.active { direction = 1 } and vim.snippet.jump(1)
+  end, { silent = true })
+
+  Map({ "i", "s" }, "<c-h>", function()
+    return vim.snippet.active { direction = -1 } and vim.snippet.jump(-1)
+  end, { silent = true })
 
   local cmp_kinds = {
     Class = "",
@@ -65,7 +95,7 @@ M.config = function()
     },
     snippet = {
       expand = function(args)
-        luasnip.lsp_expand(args.body)
+        vim.snippet.expand(args.body)
       end,
     },
     sources = {
@@ -79,7 +109,7 @@ M.config = function()
     formatting = {
       fields = { "kind", "abbr", "menu" },
       format = function(entry, vim_item)
-        local kind = lspkind.cmp_format { symbol_map = cmp_kinds } (entry, vim_item)
+        local kind = lspkind.cmp_format { symbol_map = cmp_kinds }(entry, vim_item)
         local strings = vim.split(kind.kind, "%s", { trimempty = false })
         kind.kind = " " .. strings[1] .. " "
         kind.menu = "    (" .. strings[2] .. ")"
@@ -112,36 +142,19 @@ M.config = function()
       ["<C-CR>"] = cmp.mapping.complete {
         reason = cmp.ContextReason.Auto,
       },
-      ["<C-y>"] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
+      ["<C-y>"] = cmp.mapping(
+        cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
+        { "i", "c" }
+      ),
       -- navigate completion menu
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
-      -- navigate snippet positions
-      ["<C-l>"] = cmp.mapping(function()
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<C-h>"] = cmp.mapping(function()
-        if luasnip.locally_jumpable(-1) then
-          luasnip.jump(-1)
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      -- toggle choices of choice nodes
       ["<C-n>"] = cmp.mapping(function()
-        if luasnip.choice_active() then
-          luasnip.change_choice(1)
+        if ls.choice_active() then
+          ls.change_choice(1)
         end
       end, {
         "i",
