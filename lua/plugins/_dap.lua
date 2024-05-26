651d926765c79bd04815157ee9bba32efd745a99
koopa1338 <sinner1991@gmail.com>
2024-05-26 22:05:36 +0200

    improved debugger keybindings

--- lua/plugins/_dap.lua
+++ lua/plugins/_dap.lua
@@ -70,51 +70,60 @@ M.config = function()
         dap_v.setup {}
       end)
 
-      Map("n", "<M-d>b", dap.toggle_breakpoint, { silent = true, desc = "Toggle Debug Breakpoint" })
+      Map("n", "<F1>b", dap.toggle_breakpoint, { silent = true, desc = "Toggle Debug Breakpoint" })
       Map(
         "n",
-        "<M-d><M-l>",
+        "<F1><M-l>",
         "<cmd>Telescope dap list_breakpoints layout_strategy=horizontal<CR>",
         { silent = true, desc = "Show Debug Breakpoints" }
       )
-      Map("n", "<M-d>c", function()
+      Map("n", "<F1>c", function()
         vim.ui.input({ prompt = "Breakpoint condition: " }, function(input)
           dap.set_breakpoint(input)
         end)
       end, { silent = true, desc = "Set Debug Breakpoint Condition" })
-      Map("n", "<M-d>m", function()
+      Map("n", "<F1>m", function()
         vim.ui.input({ prompt = "Log Point message: " }, function(input)
           dap.set_breakpoint(nil, nil, input)
         end)
       end, { silent = true, desc = "Set Debug Log Point Message" })
 
-      Map("n", "<M-d>C", dap.clear_breakpoints, { silent = true, desc = "Clear All Breakpoints" })
+      Map("n", "<F1><M-c>", dap.clear_breakpoints, { silent = true, desc = "Clear All Breakpoints" })
 
       Map("n", "<Space>?", function()
         dapui.eval(nil, { enter = true })
       end, { silent = true, desc = "Display value under cursor" })
 
-      Map("n", "<F12>", function()
-        if dap.session() then
-          dap.terminate()
-        else
-          vim.cmd "Telescope dap configurations theme=dropdown layout_strategy=horizontal"
-        end
-      end, { silent = true, desc = "Start/Terminate Debugger Session" })
-      Map("n", "<F11>", dap.pause, { silent = true, desc = "Pause Debugger" })
-
-      Map("n", "<F5>", dap.step_over, { silent = true, desc = "Debug Step Over" })
       Map("n", "<F4>", dap.run_to_cursor, { silent = true, desc = "Debug GoTo" })
+      Map("n", "<F5>", dap.step_over, { silent = true, desc = "Debug Step Over" })
 
       Map("n", "<F6>", dap.step_into, { silent = true, desc = "Debug Step Into" })
       Map("n", "<F7>", dap.step_out, { silent = true, desc = "Debug Step Out" })
 
+      Map("n", "<F11>", dap.pause, { silent = true, desc = "Pause Debugger" })
+
+      Map("n", "<F12>", function()
+        if dap.session() then
+          vim.ui.select({ "Terminate", "Restart", "Disconnect" }, { prompt = "Session Actions" }, function(choice)
+            if choice == "Terminate" then
+              dap.terminate()
+            elseif choice == "Restart" then
+              dap.restart()
+            elseif choice == "Disconnect" then
+              dap.disconnect()
+            else
+              vim.notify("No valid session action selected", vim.log.levels.WARN, { title = "Error on session action" })
+            end
+          end)
+        else
+          vim.cmd "Telescope dap configurations theme=dropdown layout_strategy=horizontal"
+        end
+      end, { silent = true, desc = "Start/Terminate Debugger Session" })
+
       Map("n", "<M-d>k", dap.up, { silent = true, desc = "Debug Step Up" })
       Map("n", "<M-d>j", dap.down, { silent = true, desc = "Debug Step Down" })
 
-      Map("n", "<F1>", dap.restart, { silent = true, desc = "Restart Debugger" })
       Map("n", "<M-d>r", dap.repl.toggle, { silent = true, desc = "Toggle Debug REPL" })
-      Map("n", "<M-d>q", dap.disconnect, { silent = true, desc = "Disconnect Debugger" })
     end)
   end)
 end
