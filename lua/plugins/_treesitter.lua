f1550f8e5606ce2b69644672fe92c6ee2f048eb9
koopa1338 <sinner1991@gmail.com>
2025-07-29 03:49:05 +0200

    add argument swap bindings back to treesitter textobjects

--- lua/plugins/_treesitter.lua
+++ lua/plugins/_treesitter.lua
@@ -1,79 +1,21 @@
-local M = {
+return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "nvim-lua/popup.nvim",
-    "windwp/nvim-ts-autotag",
+    {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "main",
+      config = function()
+        local swap = require "nvim-treesitter-textobjects.swap"
+        Map("n", "<M-s>", function()
+          swap.swap_next "@parameter.inner"
+        end, { silent = true })
+        Map("n", "<leader><M-s>", function()
+          swap.swap_previous "@parameter.outer"
+        end, { silent = true })
+      end,
+    },
   },
-  event = "VeryLazy",
 }
-
-M.config = function()
-  L("nvim-treesitter.configs", function(config)
-    config.setup {
-      auto_install = true,
-      -- installed by default: lua, query, vimdoc, vim, c, python, bash, markdown, markdown_inline
-      ensure_installed = {
-        "rust",
-        "html",
-        "css",
-        "scss",
-        "json",
-        "typescript",
-        "cpp",
-        "cmake",
-        "diff",
-        "dockerfile",
-        "toml",
-        "make",
-      },
-      highlight = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          -- mappings for incremental selection (visual mappings)
-          init_selection = "<M-w>", -- maps in normal mode to init the node/scope selection
-          scope_incremental = "<M-w>", -- increment to the upper scope (as defined in locals.scm)
-          node_incremental = "<M-n>", -- increment to the upper named parent
-          node_decremental = "<M-p>", -- decrement to the previous node
-        },
-      },
-      indent = { enable = true },
-      refactor = {
-        highlight_definitions = { enable = true },
-        highlight_current_scope = { enable = true },
-      },
-      autotags = {
-        enable = true,
-      },
-      textobjects = {
-        lsp_interop = {
-          enable = true,
-          border = vim.g.border_type,
-          peek_definition_code = {
-            ["<leader>pf"] = "@function.outer",
-            ["<leader>pc"] = "@class.outer",
-            ["<leader>ps"] = "@block.outer",
-          },
-        },
-        "none",
-        select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<M-s>"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader><M-s>"] = "@parameter.inner",
-          },
-        },
-      },
-    }
-  end)
-end
-
-return M
