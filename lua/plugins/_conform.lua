7c0a98abed86f6860139f6c674f9004a5fbb8165
koopa1338 <sinner1991@gmail.com>
2026-08-12 15:29:22 +0200

    configure conform and fix formatting

--- lua/plugins/_conform.lua
+++ lua/plugins/_conform.lua
@@ -2,14 +2,15 @@ local M = {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      lua = { "stylua" },
       javascript = { "prettier", stop_after_first = true, lsp_format = "fallback" },
       typescript = { "prettier", stop_after_first = true, lsp_format = "fallback" },
       typescriptreact = { "prettier", stop_after_first = true, lsp_format = "fallback" },
+      lua = { "stylua" },
+      python = { "black", lsp_format = "fallback" },
+      go = { lsp_format = "last" },
+      gitcommit = { "editorconfig", lsp_format = "never", format_opts = { cols = 72 } },
     },
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
+    default_format_opts = {},
   },
 }
 
